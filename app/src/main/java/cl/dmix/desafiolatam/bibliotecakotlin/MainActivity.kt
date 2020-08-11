269295dc27e7b43c319b878f26582c03c412c277
David Mix <mixelectronico@gmail.com>
2020-08-11 17:10:04 -0400

    Primer Commit :)

--- app/src/main/java/cl/dmix/desafiolatam/bibliotecakotlin/MainActivity.kt
+++ app/src/main/java/cl/dmix/desafiolatam/bibliotecakotlin/MainActivity.kt
@@ -15,6 +15,17 @@ class MainActivity : AppCompatActivity() {
         var libro2 = LibrosEscolares("Clases de Kotlin",
             2345687891, 2020, "FuentesEditorial",
             85,8000, "Cristian",true)
+        var libro3 = LibrosEscolares("Android instead of IOS",
+            3456878912, 2010,
+            "Los PRO", 10, 1000,"Anónimo", true)
+        val libro4 = LibrosEscolares("Estructuras de datos y algoritmos en JAVA",
+            456789123, 2000, "Java4Dummyes", 1500,
+            80000, "Uncle Bob", false)
+
+        libro1.imprimir()
+        libro2.imprimir()
+        libro3.imprimir()
+        libro4.imprimir()
         
     }
 }
